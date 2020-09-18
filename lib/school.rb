13a6bcef72117be81c431415f9696b572f30d6f1
Percy-gif <percymajor@gmail.com>
2020-09-17 23:00:43 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+#QUESTION - HOW CAN I USE BINDING.PRY TO HELP ME HERE? 
+
+class School 
+    @@roster = {}
+ 
+    def initialize(school)
+        @school = school 
+        @roster = []
+    end 
+
+    def school 
+        @school
+    end 
+
+    def roster
+        @roster 
+    end 
+
+   
+    def add_student(name, grade)
+        @roster << roster 
+    end 
+
+    def grade 
+    end 
+
+    def sort 
+    end 
+
+
+end 
